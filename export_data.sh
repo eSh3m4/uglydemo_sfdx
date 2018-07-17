5f7c2abe1fb24afcc6be1bbd96e9c44f61c6252f
arieunier <arieunier@salesforce.com>
2018-07-17 17:29:45 +0200

    Eramet

--- export_data.sh
+++ export_data.sh
@@ -12,3 +12,5 @@ SCRATCHORGALIAS=$1
 sfdx force:data:tree:export --query "select Id,Description__c, image__c , name, externalid__c from participant__c"  --prefix sfdx-demo-data --outputdir ./data --plan -u $SCRATCHORGALIAS
 sfdx force:data:tree:export --query "SELECT IsActive,Description, ProductCode, Family, image__c, Name, productPage__c  FROM Product2" --prefix sfdx-demo-data --outputdir ./data --plan -u $SCRATCHORGALIAS
 sfdx force:data:tree:export --query "select Name, active__c, externalid__c from GameActivity__c"  --prefix sfdx-demo-data --outputdir ./data --plan  -u $SCRATCHORGALIAS
+
+sfdx force:data:tree:export --query "select Name, Mineral_Deposit__c, externalid__c from Mine__c" --prefix sfdx-demo-data --outputdir ./data --plan  -u $SCRATCHORGALIAS
